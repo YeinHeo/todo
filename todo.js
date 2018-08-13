e06b3e001823d70f4fdaaf4585335e7af59c59a7
YeinHeo <YeinHeo95@google.com>
2018-08-13 19:39:29 +0900

    2

--- todo.js
+++ todo.js
@@ -1,20 +1,49 @@
+
+function deleteUpdate(){
+  var buttonID = this.id.replace("button_", "");
+  var itemText = document.getElementById("list_" + buttonID);
+
+  this.parentNode.remove();
+}
+
 var create_li_event = function () {
+
+  itemNum++;
+
   var $input = document.querySelector('input');
   var result = document.querySelector('.result');
 
   var $li = document.createElement('li');
-  $li.innerText = $input.value;
+
+  var span = document.createElement("span");
+  var $button = document.createElement('button') ;
+  $button.innerText='delete';
+
+  span.id="list_"+itemNum;
+  $button.id="button_"+itemNum;
+
+  $button.onclick=deleteUpdate;
+
+  span.innerText = $input.value;
+
+  //span.innerHtml = '';
+
+  $li.appendChild(span);
+  $li.appendChild($button);
+  console.log($li);
 
   result.appendChild($li);
+
   $input.value = '';
 }
 
 // var $button = document.querySelector('button');
 // $button.addEventListener('click', create_li_event);
 
+var itemNum = 0;
+
 var $input = document.querySelector('input');
 $input.addEventListener('keydown', function (e) {
   if (e.keyCode !== 13) return;
   create_li_event();
 })
-console.log()
